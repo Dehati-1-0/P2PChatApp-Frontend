bd2e02a169af1272493767a3cc5b3846969ccc61
psd-99 <pasinduekanayaka99@gmail.com>
2024-07-28 02:46:47 +0530

    settings page

---
+++ lib/Screens/settings.dart
@@ -0,0 +1,152 @@
+import 'package:Dehati/widgets/custom_app_bar.dart';
+import 'package:flutter/material.dart';
+
+class SettingsPage extends StatelessWidget {
+  const SettingsPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: CustomAppBar(),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(40.0, 10.0, 40.0, 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Text(
+              "Settings",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.w800),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: const Color.fromARGB(62, 22, 26, 137).withOpacity(0.03),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      children: [
+                        CircleAvatar(
+                          backgroundImage: AssetImage(
+                              'assets/current_user.jpg'), // Replace with the current user's image
+                          radius: 30,
+                        ),
+                        SizedBox(
+                          width: 30,
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pushNamed(context, '/phrase');
+                          },
+                          child: Text(
+                            "Change the profile picture",
+                            style: TextStyle(
+                                decoration: TextDecoration.underline,
+                                fontWeight: FontWeight.w400),
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Text(
+                      "Username",
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Row(
+                      children: [
+                        Text(
+                          "Jhon Smith",
+                          style: TextStyle(fontSize: 14),
+                        ),
+                        SizedBox(
+                          width: 150,
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.pushNamed(context, '/phrase');
+                          },
+                          child: Text(
+                            "Edit",
+                            style: TextStyle(
+                                decoration: TextDecoration.underline,
+                                fontWeight: FontWeight.w400),
+                          ),
+                        )
+                      ],
+                    ),
+                    Container(
+                      height: 1,
+                      width: 500,
+                      color: Colors.grey,
+                    ),
+                    SizedBox(
+                      height: 30,
+                    ),
+                    Text(
+                      "ID",
+                      style:
+                          TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+                    ),
+                    SizedBox(
+                      height: 8,
+                    ),
+                    Text("PPSSJN123"),
+                    Container(
+                      height: 1,
+                      width: 500,
+                      color: Colors.grey,
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+
+//button
+
+                    GestureDetector(
+                      onTap: () {
+                        Navigator.pushNamed(context, '');
+                      },
+                      child: Center(
+                        child: Container(
+                          height: 70,
+                          width: 338,
+                          padding: const EdgeInsets.all(15),
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(10),
+                            color: const Color(0xFF1A2247),
+                          ),
+                          child: const Center(
+                            child: const Text(
+                              "Logout",
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
