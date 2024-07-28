4e0d607a8c52eb233f3f3bd1e5d5852071910272
psd-99 <pasinduekanayaka99@gmail.com>
2024-07-28 09:02:38 +0530

    login page

--- lib/Screens/login.dart
+++ lib/Screens/login.dart
@@ -5,72 +5,140 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      backgroundColor: Colors.white,
+      appBar: CustomAppBar(),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(40.0, 10, 40, 10),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Spacer(),
             Image.asset(
-              'assets/logo.png', // Make sure the path is correct
-              height: 100, // Adjust the height as needed
+              'assets/logo.png',
+              height: 80,
             ),
-            SizedBox(height: 20),
+            SizedBox(
+              height: 40,
+            ),
             Text(
-              'DEHATI', 
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-                color: Colors.black,
-              ),
+              "Enter Your phrase here",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
             ),
-            Spacer(),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 32.0),
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Color(0xFF0A174E), // Background color
-                  padding: EdgeInsets.symmetric(vertical: 16.0),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(8.0),
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              height: 70,
+              decoration: BoxDecoration(
+                  color: Color.fromARGB(61, 135, 136, 144).withOpacity(0.2),
+                  borderRadius: BorderRadius.circular(10)),
+              child: Center(
+                child: TextField(
+                  decoration: InputDecoration(
+                    fillColor: Colors.transparent,
+                    hintText: 'Enter Your phrase here',
+                    border: InputBorder.none,
+                    filled: true,
                   ),
                 ),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/phrase');
-                },
-                child: Center(
-                  child: Text(
-                    'LOGIN',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+
+//button
+
+            GestureDetector(
+              onTap: () {
+                Navigator.pushNamed(context, '/phrase');
+              },
+              child: Center(
+                child: Container(
+                  height: 70,
+                  width: 338,
+                  padding: const EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: const Color(0xFF1A2247),
+                  ),
+                  child: const Center(
+                    child: const Text(
+                      "Login",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600),
                     ),
                   ),
                 ),
               ),
             ),
-            SizedBox(height: 10),
-            Text(
-              'or',
-              style: TextStyle(
-                color: Colors.grey,
-                fontSize: 14,
-              ),
+            SizedBox(
+              height: 30,
             ),
-            TextButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/keyphrase');
-                   
-              },
-              child: Text(
-                'Generate unique key phrase',
-                style: TextStyle(
-                  color: Colors.blue[900],
+            Container(
+              height: 1,
+              width: 500,
+              color: Color.fromARGB(61, 135, 136, 144).withOpacity(0.3),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Center(
+                child: Text(
+              "Or",
+              style: TextStyle(
                   fontSize: 16,
+                  fontWeight: FontWeight.w600,
+                  color: Color.fromARGB(61, 135, 136, 144).withOpacity(0.3)),
+            )),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              "Don't have a phrase?",
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+            ),
+            SizedBox(
+              height: 30,
+            ),
+            //button
+
+            GestureDetector(
+              onTap: () {
+                Navigator.pushNamed(context, '/keyphrase');
+              },
+              child: Center(
+                child: Container(
+                  height: 70,
+                  width: 338,
+                  padding: const EdgeInsets.all(15),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: const Color(0xFF1A2247),
+                  ),
+                  child: const Center(
+                    child: const Text(
+                      "Generate a unique key phrase",
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ),
                 ),
               ),
             ),
-            SizedBox(height: 20),
+            SizedBox(
+              height: 30,
+            ),
+            Text(
+              "centuries, but also the leap into electronic typesetting, remaining",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontWeight: FontWeight.w500,
+              ),
+            ),
           ],
         ),
       ),
