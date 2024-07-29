7e8321a203bbff803d79ff3ba8367888e2f1bf8c
psd-99 <pasinduekanayaka99@gmail.com>
2024-07-29 16:45:01 +0530

    modified key phrase page

--- lib/Screens/key_phrase_page.dart
+++ lib/Screens/key_phrase_page.dart
@@ -12,7 +12,7 @@ class KeyPhrasePage extends StatelessWidget {
         child: Column(
           children: [
             const Text(
-              "Why we need a unique key phrase?",
+              "Why We Need Your ID & Key Phrase?",
               textAlign: TextAlign.center,
               style: TextStyle(fontSize: 20, fontWeight: FontWeight.w800),
             ),
@@ -20,18 +20,59 @@ class KeyPhrasePage extends StatelessWidget {
               height: 20,
             ),
             Container(
+              height: 400,
               decoration: BoxDecoration(
                 color: const Color.fromARGB(62, 22, 26, 137).withOpacity(0.03),
                 borderRadius: BorderRadius.circular(10),
               ),
-              child: const Padding(
-                padding: EdgeInsets.all(10.0),
-                child: Text(
-                  'The key phrase is crucial for maintaining the security and continuity of your identity on Dehati App. Generated uniquely for your device, it allows you to recover your original ID when switching devices or logging in from a different one, ensuring your identity remains recognizable. Unlike traditional recovery methods, the key phrase is a secure and simple way to verify your identity, making impersonation impossible. It does not restore saved contacts or messages, protecting your privacy and preventing unauthorized access. Remembering this key phrase simplifies account recovery and management, making it an essential part of securing your Dehati App account.',
-                  textAlign: TextAlign.justify,
-                  style: TextStyle(
-                    fontWeight: FontWeight.w800,
-                    fontSize: 16,
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: SingleChildScrollView(
+                  child: Text.rich(
+                    TextSpan(
+                      children: [
+                        TextSpan(
+                          text:
+                              "Before using Dehati App, we ask for your unique ID, key phrase, and username to ensure a secure and personalized experience:\n\n",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.black,
+                          ),
+                        ),
+                        TextSpan(
+                          text: "Unique ID:\n",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        TextSpan(
+                          text:
+                              "This serves as your digital identity, helping others find you while keeping your personal details private.\n\n",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.black,
+                          ),
+                        ),
+                        TextSpan(
+                          text: "Key Phrase:\n",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        TextSpan(
+                          text:
+                              "A unique phrase for security, allowing you to recover your ID when changing or logging into a new device. This maintains your identity without recovering old contacts.",
+                          style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.black,
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
