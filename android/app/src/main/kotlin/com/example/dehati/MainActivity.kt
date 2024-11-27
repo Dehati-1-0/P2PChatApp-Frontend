4f592ff4e0a83f61a7efafe6ae909569d941a82b
Nihara Jayatilleke <nihara.jayatilleke@gmail.com>
2024-11-27 09:38:22 +0530

    edit username

--- android/app/src/main/kotlin/com/example/dehati/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/dehati/MainActivity.kt
@@ -15,7 +15,7 @@ import java.net.DatagramSocket
 import java.net.InetAddress
 import java.net.ServerSocket
 import java.io.BufferedReader
-import java.io.InputStreamReader
+import java.io.InputStreamReader 
 import java.io.PrintWriter
 
 data class DiscoveredDevice(val ip: String, val modelName: String)
